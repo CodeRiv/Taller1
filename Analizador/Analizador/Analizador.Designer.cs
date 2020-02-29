c85d20ab45d7f231f7d4a4a12b77fc07599183da
brianrivera1604 <rivera.brian99@gmail.com>
2020-02-29 00:05:43 -0600

    Add files via upload

---
+++ Analizador/Analizador/Analizador.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Analizador
+{
+    partial class Analizador
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtExpresion = new System.Windows.Forms.TextBox();
+            this.btnAnalizar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.dgvResultados = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtExpresion
+            // 
+            this.txtExpresion.Location = new System.Drawing.Point(12, 30);
+            this.txtExpresion.Name = "txtExpresion";
+            this.txtExpresion.Size = new System.Drawing.Size(781, 32);
+            this.txtExpresion.TabIndex = 1;
+            // 
+            // btnAnalizar
+            // 
+            this.btnAnalizar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAnalizar.Location = new System.Drawing.Point(338, 68);
+            this.btnAnalizar.Name = "btnAnalizar";
+            this.btnAnalizar.Size = new System.Drawing.Size(163, 60);
+            this.btnAnalizar.TabIndex = 3;
+            this.btnAnalizar.Text = "Realizar división";
+            this.btnAnalizar.UseVisualStyleBackColor = false;
+            this.btnAnalizar.Click += new System.EventHandler(this.btnAnalizar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnLimpiar.Location = new System.Drawing.Point(306, 333);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(232, 83);
+            this.btnLimpiar.TabIndex = 4;
+            this.btnLimpiar.Text = "Borrar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // dgvResultados
+            // 
+            this.dgvResultados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResultados.Location = new System.Drawing.Point(112, 134);
+            this.dgvResultados.Name = "dgvResultados";
+            this.dgvResultados.RowHeadersWidth = 51;
+            this.dgvResultados.Size = new System.Drawing.Size(604, 183);
+            this.dgvResultados.TabIndex = 5;
+            // 
+            // Analizador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Analizador.Properties.Resources.zoaarts181000037;
+            this.ClientSize = new System.Drawing.Size(805, 428);
+            this.Controls.Add(this.dgvResultados);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnAnalizar);
+            this.Controls.Add(this.txtExpresion);
+            this.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Analizador";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Analizador Léxico";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtExpresion;
+        private System.Windows.Forms.Button btnAnalizar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.DataGridView dgvResultados;
+    }
+}
+
---
+++ Analizador/Analizador/Analizador.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Analizador
+{
+    public partial class Analizador : Form
+    {
+        //Primero  definimos listas de caracteres
+        List<char> _num = new List<char>(new char[] { '0','1','2','3','4','5','6','7','8','9'});
+        List<char> _var = new List<char>(new char[] { 'A', 'B', 'C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z'});
+        List<char> _ope = new List<char>(new char[] { '+', '-', '*', '/' });
+        List<char> _del = new List<char>(new char[] { '(', ')' });
+        DataTable _tblRes = new DataTable();
+
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {   //Crea columnas a las tablas
+            _tblRes.Columns.Add("Token", typeof(char));
+            _tblRes.Columns.Add("Tipo", typeof(string));
+        }      
+
+        private void btnAnalizar_Click(object sender, EventArgs e)
+        {
+            //Limpia la tabla
+            _tblRes.Clear();
+            //Crea resultados en la función
+            realizarAnalisis();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {   //Limpia la tabla.
+            txtExpresion.Clear();
+            _tblRes.Clear();
+            //Limpia la base de datos.
+            dgvResultados.DataSource = null;
+            dgvResultados.Refresh();
+        }
+
+        public void realizarAnalisis()
+        {// Divide cada caracter a elementos, adicionalmente, elimina los espacios
+            List<char> _elementos = txtExpresion.Text.Replace(" ", "").ToCharArray().ToList();
+            //Verifica que haya al menos un elemento
+            if (_elementos.Count > 0)
+            {   
+                DataRow _fila;
+                //Para cada elemento revisa de que se trata
+                foreach (char elemento in _elementos)
+                {
+                    _fila = _tblRes.NewRow();
+
+                    if (_num.Contains(elemento))
+                    {
+                        _fila["Token"] = elemento;
+                        _fila["Tipo"] = "Número";
+                    }
+                    else if (_var.Contains(elemento.ToString().ToUpper()[0]))
+                    {
+                        _fila["Token"] = elemento;
+                        _fila["Tipo"] = "Variable";
+                    }
+                    else if (_ope.Contains(elemento))
+                    {
+                        _fila["Token"] = elemento;
+                        _fila["Tipo"] = "Operador";
+                    }
+                    else if (_del.Contains(elemento))
+                    {
+                        _fila["Token"] = elemento;
+                        _fila["Tipo"] = "Delimitador";
+                    }
+                    else
+                    {
+                        _fila["Token"] = elemento;
+                        _fila["Tipo"] = "Error";
+                    }
+
+                    _tblRes.Rows.Add(_fila);
+                }
+
+                dgvResultados.DataSource = _tblRes;
+                dgvResultados.Refresh();
+            }
+            else
+            {
+                dgvResultados.DataSource = null;
+                dgvResultados.Refresh();
+            }
+        }
+        public Analizador()
+        {
+            InitializeComponent();
+        }
+    }
+}
